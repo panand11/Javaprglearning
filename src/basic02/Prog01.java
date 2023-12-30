bde6054012549046150f0816d5332c9726657ca4
panand11 <panand.sap@gmail.com>
2023-12-30 12:33:58 +0530

    added code in prog01

--- src/basic02/Prog01.java
+++ src/basic02/Prog01.java
@@ -1,5 +1,14 @@
 package src.basic02;
 
 public class Prog01 {
+    public static void main(String[] args) {
+        int a=10;
+        float b= 100;
+        double c;
+        c= a%b;
+        char ch = 67;
+        System.out.println("result = " +c +'\t' + "Char value "+ch);
+
+    }
 
 }
