0c45951629defda426d288f87ce96fbcf4e6fc15
panand11 <panand.sap@gmail.com>
2023-12-30 12:49:52 +0530

    added code in prog02 to test if condition example

---
+++ src/basic02/Prog02.java
@@ -0,0 +1,13 @@
+package src.basic02;
+
+public class Prog02 {
+    public static void main(String[] args) {
+        boolean a = true;
+        a = !a;
+        if(2+2 >= 4){
+            System.out.println("Inside the loop code will be execute when the condition is true");
+        }
+
+        System.out.println("This is outside the loop, it will be executed always");
+    }
+}
